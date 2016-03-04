4184edf3e111308c5dc13494eb3b89bd26272505
rainsilence0911 <rainsilence@qq.com>
2016-03-04 14:23:51 +0800

    support mobile

--- js/manager/dragMgr.js
+++ js/manager/dragMgr.js
@@ -6,9 +6,14 @@ air.define("air.management.DragManager", function() {
     var mouseDown, lastMouseX, lastMouseY;
     
     function handleMouseDown(event) {
+        
+        if (event.type === "touchstart" && event.touches.length !== 1) {
+            return;
+        }
+        
         mouseDown = true;
-        lastMouseX = event.clientX;
-        lastMouseY = event.clientY;
+        lastMouseX = event.clientX || event.touches[0].clientX;
+        lastMouseY = event.clientY || event.touches[0].clientY;
         
         event.preventDefault();
     }
@@ -25,11 +30,13 @@ air.define("air.management.DragManager", function() {
 
     function handleMouseMoveWrapper(mouseMoveHandler) {
         return function(event) {
-            if (!mouseDown) {
+            if (!mouseDown ||
+                (event.type === "touchmove" && event.touches.length !== 1)) {
                 return;
             }
-            var newX = event.clientX;
-            var newY = event.clientY;
+            
+            var newX = event.clientX || event.touches[0].clientX;
+            var newY = event.clientY || event.touches[0].clientY;
 
             var deltaX = newX - lastMouseX
             var deltaY = newY - lastMouseY;
@@ -42,11 +49,18 @@ air.define("air.management.DragManager", function() {
             event.preventDefault();
         };
     }
+    
     return {
         register: function(domRef, mouseMoveHandler) {
-            domRef.onmousedown = handleMouseDown;
-            doc.onmouseup = handleMouseUp;
-            doc.onmousemove = handleMouseMoveWrapper(mouseMoveHandler);
+            domRef.addEventListener("mousedown", handleMouseDown, false);
+            domRef.addEventListener("touchstart", handleMouseDown, false);
+            
+            doc.addEventListener("touchend", handleMouseUp, false);
+            doc.addEventListener("mouseup", handleMouseUp, false);
+            
+            var mouseMoveProxy = handleMouseMoveWrapper(mouseMoveHandler);
+            doc.addEventListener("touchmove", mouseMoveProxy, false);
+            doc.addEventListener("mousemove", mouseMoveProxy, false);
         }
     };
 });
